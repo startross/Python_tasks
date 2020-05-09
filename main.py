f6db895bf97ab250cb98b66b8ccd265d970ce785
startross <64436858+startross@users.noreply.github.com>
2020-05-09 12:37:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import user_interface;
 import user_menu;
 import admin_menu;
 
-user_tp = user_interface.user_type();
-flag = 100;
-if (user_tp == 'admin'):
-    while (flag != 3):
-        flag = admin_menu.menu();
+user_type = user_interface.user_type();
+number_of_menu = 100;
+if (user_type == 'admin'):
+    while (number_of_menu != 3):
+        number_of_menu = admin_menu.menu();
 else:
-    while (flag != 4):
-        flag = user_menu.menu(user_tp);
-        if (flag == 0):
+    while (number_of_menu != 4):
+        number_of_menu = user_menu.menu(user_type);
+        if (number_of_menu == 0):
             print("Error");
             break;
